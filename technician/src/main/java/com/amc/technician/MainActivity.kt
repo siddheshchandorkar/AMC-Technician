1003964caf0f2928da3b21afaf70c1a93160240b
siddhesh_chandorkar <siddhesh.chandorkar@paynearby.in>
2022-11-05 23:31:19 +0530

    logo integrated

--- technician/src/main/java/com/amc/technician/MainActivity.kt
+++ technician/src/main/java/com/amc/technician/MainActivity.kt
@@ -1,19 +1,15 @@
 package com.amc.technician
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.amc.dashboard.DashBoardActivity
-import com.amc.login.presentation.ui.LoginActivity
+import androidx.appcompat.app.AppCompatActivity
 import com.amc.technician.presentation.AppNavigation
-import kotlinx.coroutines.CoroutineScope
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        AppNavigation.navigateToLogin(this)
+        AppNavigation.navigateToDashboard(this)
         finish()
     }
 }
---
+++ technician/src/main/java/com/amc/technician/TechnicianApplication.kt
@@ -0,0 +1,9 @@
+package com.amc.technician
+
+import android.app.Application
+
+class TechnicianApplication : Application() {
+
+
+
+}
